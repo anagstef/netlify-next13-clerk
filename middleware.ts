cc60fd9abf3b1a58746114158516551f69c56373
Stefanos Anagnostou <stefanos@clerk.dev>
2023-04-07 20:00:32 +0300

    Test middleware

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,17 @@ import { withClerkMiddleware } from "@clerk/nextjs/server";
 import { MiddlewareRequest } from "@netlify/next";
 import type { NextRequest } from 'next/server'
 
-export default withClerkMiddleware((req: NextRequest) => {
-    console.log('Middleware running');
-    const request = new MiddlewareRequest(req as any);
-    return request.next();
-});
+// export default withClerkMiddleware((req: NextRequest) => {
+//     console.log('Middleware running');
+//     const request = new MiddlewareRequest(req as any);
+//     return request.next();
+// });
 
-// Stop Middleware running on static files
-export const config = { matcher: '/((?!_next/image|_next/static|favicon.ico).*)' };
+// // Stop Middleware running on static files
+// export const config = { matcher: '/((?!_next/image|_next/static|favicon.ico).*)' };
+
+export async function middleware(nextRequest: NextRequest) {
+    console.log('Middleware running');
+    const request = new MiddlewareRequest(nextRequest as any);
+    return request.next();
+}
