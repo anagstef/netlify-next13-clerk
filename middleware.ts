65dc2f89c85c3c7aa47c20d15ef6dab5374e84fb
Stefanos Anagnostou <stefanos@clerk.dev>
2023-04-07 20:06:38 +0300

    Use MiddlewareResponse

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 import { withClerkMiddleware } from "@clerk/nextjs/server";
-import { MiddlewareRequest } from "@netlify/next";
+import { MiddlewareResponse } from "@netlify/next";
 import type { NextRequest } from 'next/server'
 
 // export default withClerkMiddleware((req: NextRequest) => {
@@ -11,8 +11,13 @@ import type { NextRequest } from 'next/server'
 // // Stop Middleware running on static files
 // export const config = { matcher: '/((?!_next/image|_next/static|favicon.ico).*)' };
 
-export async function middleware(nextRequest: NextRequest) {
+// export async function middleware(nextRequest: NextRequest) {
+//     console.log('Middleware running');
+//     const request = new MiddlewareRequest(nextRequest as any);
+//     return MiddlewareResponse.next();
+// }
+
+export default withClerkMiddleware((req: NextRequest) => {
     console.log('Middleware running');
-    const request = new MiddlewareRequest(nextRequest as any);
-    return request.next();
-}
+    return MiddlewareResponse.next();
+});
