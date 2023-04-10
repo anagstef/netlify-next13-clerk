289debbf71d63fabaef82cc5aaf2bb4ce2a23d6b
Stefanos Anagnostou <stefanos@clerk.dev>
2023-04-10 14:04:29 +0300

    Use Clerk's nextjs matcher

--- middleware.ts
+++ middleware.ts
@@ -23,3 +23,6 @@ export default withClerkMiddleware((req: NextRequest) => {
     console.log('Middleware userId', userId);
     return NextResponse.next();
 });
+
+// Stop Middleware running on static files
+export const config = { matcher: '/((?!_next/image|_next/static|favicon.ico).*)' };
