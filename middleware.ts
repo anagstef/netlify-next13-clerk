9077cd6938e1c26cfa7a8a03451aebd8a05ccdb6
Stefanos Anagnostou <stefanos@clerk.dev>
2023-04-10 14:26:35 +0300

    Add pages and protection

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,32 @@
 import { getAuth, withClerkMiddleware } from "@clerk/nextjs/server";
-import { MiddlewareRequest, MiddlewareResponse } from "@netlify/next";
 import { NextRequest, NextResponse } from 'next/server'
 
-// export default withClerkMiddleware((req: NextRequest) => {
-//     console.log('Middleware running');
-//     const request = new MiddlewareRequest(req as any);
-//     return request.next();
-// });
+// Set the paths that don't require the user to be signed in
+const publicPaths = ['/', '/sign-in*', '/sign-up*']
 
-// // Stop Middleware running on static files
-// export const config = { matcher: '/((?!_next/image|_next/static|favicon.ico).*)' };
+const isPublic = (path: string) => {
+    return publicPaths.find(x =>
+        path.match(new RegExp(`^${x}$`.replace('*$', '($|/)')))
+    )
+}
 
-// export async function middleware(nextRequest: NextRequest) {
-//     console.log('Middleware running');
-//     const request = new MiddlewareRequest(nextRequest as any);
-//     return MiddlewareResponse.next();
-// }
-
-export default withClerkMiddleware((req: NextRequest) => {
+export default withClerkMiddleware((request: NextRequest) => {
     console.log('Middleware running');
-    const { userId } = getAuth(req);
+    if (isPublic(request.nextUrl.pathname)) {
+        return NextResponse.next()
+    }
+    // if the user is not signed in redirect them to the sign in page.
+    const { userId } = getAuth(request);
     console.log('Middleware userId', userId);
-    return NextResponse.next();
+
+    if (!userId) {
+        // redirect the users to /pages/sign-in/[[...index]].ts
+
+        const signInUrl = new URL('/sign-in', request.url)
+        signInUrl.searchParams.set('redirect_url', request.url)
+        return NextResponse.redirect(signInUrl)
+    }
+    return NextResponse.next()
 });
 
 // Stop Middleware running on static files
